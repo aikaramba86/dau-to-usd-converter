00134b2ffd080c06601b309ff4667e361bae8a13
aikaramba86 <resitharan.v@gmail.com>
2024-10-16 10:30:25 +0800

    Fix script loading issue

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function convertDAUtoUSD() {
     const dauAmount = document.getElementById('dauInput').value;
-    const conversionRate = 1; // Set your conversion rate here
+    const conversionRate = 1; // 850000
     const usdAmount = dauAmount * conversionRate;
     document.getElementById('result').innerText = `${dauAmount} DAU is equal to $${usdAmount} USD`;
 }
